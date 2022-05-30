502c23f1a38a41b62f7240a421fa218231811b59
SRAZKVT <sarah.mathey66666@gmail.com>
2022-05-30 17:48:56 +0200

    enhanced slightly readability of tests output

--- tests/tests.sh
+++ tests/tests.sh
@@ -6,12 +6,12 @@ backfolder=$(pwd)
 cd ${testfolder}
 
 for element in *.kf; do
-	printf "running test ${element}\n"
+	printf "[TESTS]: running test ${element}\n"
 	../kuteforth.php -sr ${element} | tee output.txt
 	basename=$(echo ${element} | cut -f 1 -d '.')
 	cmp -s output.txt ${basename}.txt
 	if [ ${?} -ne 0 ]; then
-		printf "Test ${basename} has failed\n"
+		printf "[TESTS]: Test ${basename} has failed\n"
 		failed=$( echo ${failed}+1 | bc)
 	fi
 	rm output.txt ${basename}
@@ -20,8 +20,8 @@ done
 cd ${backfolder}
 
 if [ ${failed} -ne 0 ]; then
-	printf "[${failed}] tests have failed\n"
+	printf "[TESTS]: [${failed}] tests have failed\n"
 	exit 1
 else
-	printf "All tests ran succesfully\n"
+	printf "[TESTS]: All tests ran succesfully\n"
 fi
